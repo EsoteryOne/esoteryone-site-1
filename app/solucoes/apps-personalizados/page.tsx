15fcf04d450d20af1ca8843dfe5a5d38e448d3df
Felipe Carvalho <one@esoteryone.com>
2026-04-08 22:52:41 -0300

    fix: corrigindo page apps personalizados

---
+++ app/solucoes/apps-personalizados/page.tsx
@@ -0,0 +1,7 @@
+export default function AppsPersonalizadosPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-cyan-50">
+      <h1 className="text-3xl">Apps Personalizados</h1>
+    </main>
+  );
+}
