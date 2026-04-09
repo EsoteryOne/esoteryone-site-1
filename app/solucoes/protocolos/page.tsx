7c47c752d916e2c7b9eb911c795a0e63d6e8ad22
Felipe Carvalho <one@esoteryone.com>
2026-04-08 22:57:46 -0300

    fix: corrigindo page protocolos

---
+++ app/solucoes/protocolos/page.tsx
@@ -0,0 +1,7 @@
+export default function ProtocolosPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-cyan-50">
+      <h1 className="text-3xl">Protocolos</h1>
+    </main>
+  );
+}
