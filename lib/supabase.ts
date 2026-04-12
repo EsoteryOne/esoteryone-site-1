8ac384cf7e8a6853468389802ac442dba55bfa13
Felipe Carvalho <one@esoteryone.com>
2026-04-11 22:12:00 -0300

    corrige URL do supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vmbwcikxoaanzncyblbdn.supabase.co'
+const supabaseUrl = 'https://vmbwcikxoaanzncylbdn.supabase.co'
 const supabasePublishableKey = 'sb_publishable_KiD39HesPjIm0WEv6X-6cQ_EnMtsLgy'
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
