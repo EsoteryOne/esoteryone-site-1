e2890773199171ef9d8ad2f4043fb0b843abab34
Felipe Carvalho <one@esoteryone.com>
2026-04-30 10:26:40 -0300

    clarity - site que ve o que o usuario ta fazendo - adicionado

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function LayoutRaiz({
 }>) {
   return (
     <html lang="pt-BR">
+
       {/* Google Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-ESHC13MRMW"
@@ -31,8 +32,21 @@ export default function LayoutRaiz({
         `}
       </Script>
 
+      {/* Microsoft Clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wjsq3mpty2");
+        `}
+      </Script>
+
       <body className="bg-[#030712] text-white antialiased">
         <EstruturaSite>{children}</EstruturaSite>
+
+        {/* Banner de cookies (não aparece em /lp) */}
         <BannerCookies />
       </body>
     </html>
