dc2f2d2d8169738ffb3bccda8ddc62560b2afbff
Felipe Carvalho <one@esoteryone.com>
2026-04-30 09:45:24 -0300

    analytics instalado

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import EstruturaSite from "@/app/componentes/estrutura-site";
 
@@ -14,6 +15,21 @@ export default function LayoutRaiz({
 }>) {
   return (
     <html lang="pt-BR">
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ESHC13MRMW"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ESHC13MRMW');
+        `}
+      </Script>
+
       <body className="bg-[#030712] text-white antialiased">
         <EstruturaSite>{children}</EstruturaSite>
       </body>
