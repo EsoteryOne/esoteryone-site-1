95c8465b36e0801e376fe62b343b57f616103e4d
Felipe Carvalho <one@esoteryone.com>
2026-04-30 15:53:57 -0300

    ainda tentando

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import "./globals.css";
 import EstruturaSite from "@/app/componentes/estrutura-site";
 import BannerCookies from "@/app/componentes/banner-cookies";
@@ -12,17 +12,17 @@ export const metadata: Metadata = {
 
 export default function LayoutRaiz({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-BR">
       <body className="bg-[#030712] text-white antialiased">
         <EstruturaSite>{children}</EstruturaSite>
-
         <BannerCookies />
 
-        <Script id="microsoft-clarity" strategy="afterInteractive">
+        {/* Microsoft Clarity */}
+        <Script id="clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
@@ -33,6 +33,7 @@ export default function LayoutRaiz({
         </Script>
       </body>
 
+      {/* Google Analytics (OFICIAL NEXT) */}
       <GoogleAnalytics gaId="G-ESHC13MRMW" />
     </html>
   );
